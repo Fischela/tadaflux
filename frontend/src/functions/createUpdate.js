e299afca9106e9d3dfc7bdc7307136989fc19d34
Norbert Frank <petermbafrank@gmail.com>
2021-08-11 06:07:18 +0100

    Tada Flux Upload

--- frontend/src/functions/createUpdate.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-
-export const createOrUpdateUser = async (authToken) => {
-    console.log(process.env.REACT_APP_API)
-    return await axios.post("http://localhost:8000/api/createupdateuser", {},
-        {headers:{authToken}})
-}
-
